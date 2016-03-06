c37d1d5acf01fc6cc878f65e13f15729c72e2ead
hantaom <hantaom@gmail.com>
2016-03-06 06:47:12 -0800

    hi

---
+++ Assets/Scripts/pauseMenuScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pauseMenuScript : MonoBehaviour {
+    bool moved = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void pauseMenu()
+        
+    {
+        if (!moved)
+        {
+            transform.position = new Vector3(transform.position.x - 2.89F, transform.position.y, transform.position.z);
+            moved = true;
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x + 2.89F, transform.position.y, transform.position.z);
+            moved = false;
+        }
+    }
+}
